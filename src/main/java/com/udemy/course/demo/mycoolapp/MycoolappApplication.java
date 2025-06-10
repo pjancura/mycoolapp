af8b61b9889e754d8a7f141be7a5b78ba03e936c
pjancura <jancura.paul@gmail.com>
2025-06-09 21:28:54 -0400

    initial commit

---
+++ src/main/java/com/udemy/course/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.course.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoolappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/course/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.course.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
